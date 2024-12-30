c062995e3a52ac8e763f91fc8f77f0b498dc5260
Tejas <jupitersorbeet@gmail.com>
2024-12-30 16:35:38 +0530

    milestone 5 completed

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,8 +1,10 @@
-const express = require("express");
-const { CreateUser } = require("../controllers/user.controller");
-const upload = require("../middlewares/multer");
+const express = require('express');
+const { CreateUSer } = require('../controllers/user.controller');
+const upload = require('../middlewares/multer');
+
 const router = express.Router();
 
-router.get("/create-user", upload.single("file"), CreateUser);
+router.get('/create-user', upload.single('file'), CreateUSer);
 
-module.exports = router;
+export default router;
+
